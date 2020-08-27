3f398bdee93a7b7bcd9abaa347251cf7ef79481c
Grégoire Mougeolle <gregoire@nextmap.io>
2020-08-27 16:08:53 +0200

    :construction_worker: (flask) Trying a CI build for our TinyFlask

--- flask/freeze.py
+++ flask/freeze.py
@@ -1,6 +1,9 @@
 from flask_frozen import Freezer
 from app import app
 
+app.config['FREEZER_DESTINATION'] = "../dist"
+app.config['FREEZER_REMOVE_EXTRA_FILES'] = False
+
 freezer = Freezer(app)
 
 if __name__ == '__main__':
